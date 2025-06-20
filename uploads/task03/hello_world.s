d34bb61c94853b6abae647be7c5b1ed765dd1bfc
okraus <okraus42+github@pm.me>
2025-06-20 17:43:48 +0200

    hello assembly

---
+++ uploads/task03/hello_world.s
@@ -0,0 +1,17 @@
+section      .data                     ; this is where variables are initialized
+    msg      db   "Hello, World!", 0xa ; 0xa is a newline
+    msglen   equ  $ - msg
+
+section      .text                     ; this is the code section
+    global   _start                    ; declare entry point
+_start:                                ; define entry point
+    mov rax, 1                         ; sys_write(
+    mov rdi, 1                         ;   STDOUT_FILENO,
+    mov rsi, msg                       ;   msg,
+    mov rdx, msglen                    ;   sizeof(msg)
+    syscall                            ; );
+
+    ; exit
+    mov rax, 60                        ; exit
+    mov rdi, 4242                      ;   exit_status
+    syscall                            ; );
